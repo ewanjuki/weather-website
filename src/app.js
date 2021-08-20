f79a6c48c90bf5c92b334a9663d3dc350a387167
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-20 18:37:07 +0300

    serve static assets

--- src/app.js
+++ src/app.js
@@ -1,21 +1,13 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send('<h1>Weather</h1>');
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
-
-app.get("/about", (req, res) => {
-  res.send('<h1>About</h1>');
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
-  res.send({location: 'Nairobi', forecast: '25degrees'});
+  res.send({ location: "Nairobi", forecast: "25degrees" });
 });
 
 app.listen(3000, () => console.log("Server running"));
