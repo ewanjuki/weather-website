efcac623959fb79d54bb0d6f701f5b821c63a827
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-20 18:13:15 +0300

    send html & json

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello Express!");
+  res.send('<h1>Weather</h1>');
 });
 
 app.get("/help", (req, res) => {
@@ -11,11 +11,11 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send('<h1>About</h1>');
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Today's weather.");
+  res.send({location: 'Nairobi', forecast: '25degrees'});
 });
 
 app.listen(3000, () => console.log("Server running"));
