be187e693f5181392b47700a92ed4b5e9b6e0708
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-21 18:40:20 +0300

    dynamic pages with templating

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-
 const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) =>
+  res.render("index", { title: "Weather", name: "Edwin" })
+);
+
+app.get("/about", (req, res) =>
+  res.render("about", { title: "About", name: "Edwin" })
+);
+
+app.get("/help", (req, res) => res.render("help", { message: "Helpful message" }));
 
 app.get("/weather", (req, res) => {
   res.send({ location: "Nairobi", forecast: "25degrees" });
