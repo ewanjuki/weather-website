3ba62e4b280a03daf1923d039717732bb6760f17
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-21 20:49:16 +0300

    customizing the views directory

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "../public");
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) =>
@@ -15,7 +22,9 @@ app.get("/about", (req, res) =>
   res.render("about", { title: "About", name: "Edwin" })
 );
 
-app.get("/help", (req, res) => res.render("help", { message: "Helpful message" }));
+app.get("/help", (req, res) =>
+  res.render("help", { message: "Helpful message" })
+);
 
 app.get("/weather", (req, res) => {
   res.send({ location: "Nairobi", forecast: "25degrees" });
