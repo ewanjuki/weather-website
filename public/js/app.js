b2e1d643a61c0352ec549e2a0760ca8c98a2df14
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-24 18:41:22 +0300

    user interface for entering address

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
-console.log('Client side javascript is loaded!');
+console.log("Client side javascript is loaded!");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  messageOne.textContent = "Searching...";
+  messageTwo.textContent = "";
+
+  const address = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecastData;
+      }
+    });
+  });
+});
