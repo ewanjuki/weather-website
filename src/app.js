706bcd0c5272da83cf8c893e23f86f31451f6c51
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-08-25 18:21:00 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -70,4 +71,4 @@ app.get("*", (req, res) =>
   res.render("404", { title: "404", error: "Page not found.", name: "Edwin" })
 );
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(port, () => console.log("Server running"));
